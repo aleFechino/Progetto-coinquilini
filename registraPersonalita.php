5b52477b1136ef46573877a37fb60313bb2fca1f
Kirti324 <singh.kirti@itiscuneo.eu>
2026-05-05 12:45:05 +0200

    registrazione interesssi
    inizio registrazione interessi e sistemazione piccoli errori precedenti

--- registraPersonalita.php
+++ registraPersonalita.php
@@ -5,23 +5,23 @@ include("./conf/db_config.php");
 $stmt= $conn->prepare("SELECT nomePersonalita FROM personalita");
 $stmt->execute();
 $result= $stmt->get_result(); //prendo i risultati
-$prsonalita=$result->fetch_all(MYSQLI_ASSOC);
+$personalita=$result->fetch_all(MYSQLI_ASSOC);
 ?>
 
 <div class="container mt-4">
-  <form method="POST" action="registrazioneInteressi.php">
+  <form method="POST" action="registrazioneP.php">
     <div class="d-flex flex-wrap gap-2">
       <?php foreach ($personalita as $pers): ?>
-        <input type="checkbox" class="btn-check" name="interessi[]" 
-          id="interesse_<?php echo htmlspecialchars($personalita['nomeInteresse']); ?>"
-          value="<?php echo htmlspecialchars($personalita['nomeInteresse']); ?>"
+        <input type="checkbox" class="btn-check" name="personalita[]" 
+          id="personalita_<?php echo htmlspecialchars($personalita['nomePersonalita']); ?>"
+          value="<?php echo htmlspecialchars($personalita['nomePersonalita']); ?>"
           autocomplete="off"
         >
         <label 
           class="btn btn-outline-primary" 
-          for="interesse_<?php echo htmlspecialchars($personalita['nomeInteresse']); ?>"
+          for="personalita_<?php echo htmlspecialchars($personalita['nomePersonalita']); ?>"
         >
-          <?php echo htmlspecialchars($personalita['nomeInteresse']); ?>
+          <?php echo htmlspecialchars($personalita['nomePersonalita']); ?>
         </label>
       <?php endforeach; ?>
     </div>

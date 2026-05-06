49a527ba52346fa29e9fcc85a489cce613c5f1d4
NicolaGiordano7 <nicolagiordano2007@gmail.com>
2026-05-06 09:04:56 +0200

    Create cercaCasa.php

---
+++ cercaCasa.php
@@ -0,0 +1,33 @@
+<?php
+    include('./templates/header_riservata.php');
+?>
+
+<div>
+    <form>
+        <label for="Citta">Città</label><br>
+        <input type="text" id="Citta" name="Citta" value=""><br>
+        
+        <label for="nStanzeLetto">Numero camere da letto</label><br>
+        <input type="text" id="nStanzeLetto" name="nStanzeLetto" value=""><br>
+
+        <label for="Citta">Numero bagni</label><br>
+        <input type="text" id="Citta" name="Citta" value=""><br>
+
+        <label for="Citta">Città</label><br>
+        <input type="text" id="Citta" name="Citta" value=""><br>
+
+        <label for="Citta">Città</label><br>
+        <input type="text" id="Citta" name="Citta" value=""><br>
+
+        <label for="Citta">Città</label><br>
+        <input type="text" id="Citta" name="Citta" value=""><br>
+
+        <label for="Citta">Città</label><br>
+        <input type="text" id="Citta" name="Citta" value=""><br>
+
+        <label for="Citta">Città</label><br>
+        <input type="text" id="Citta" name="Citta" value=""><br>
+
+        <input type="submit" value="Submit">
+    </form>
+</div>

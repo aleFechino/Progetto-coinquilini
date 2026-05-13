68aea694b9a730fae348052166f6171f5e55086d
icoSolaro <ico.solaro07@gmail.com>
2026-05-13 10:42:37 +0200

    nonMiPiace

--- php/nonMiPiace.php
+++ php/nonMiPiace.php
@@ -1,6 +1,12 @@
 <?php
+session_start();
 include('../conf/db_config.php');
-$idUtenteVisto=$_POST["idUtente"]
 
-$stmt = $conn->prepare("INSERT TO utente_visto_utente(idUtente, idUtenteVisto,mi_piace) VALUES (?,?,?)");
+$id_utenteVisto = $_GET['id'];
+
+
+$stmt = $conn->prepare("INSERT INTO `utente_visto_utente`(`idUtente`, `idUtenteVisto`, `mi_piace`) VALUES ('?','?','?')");
+$stmt->bind_param("sss",$_SESSION['id'], $id_utenteVisto, 1);
+$stmt->execute();
+
 ?>

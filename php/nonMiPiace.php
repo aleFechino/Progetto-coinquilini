9ff7eff646984e12e8c8b14d33d2126ec3d27d71
icoSolaro <ico.solaro07@gmail.com>
2026-05-13 11:06:19 +0200

    Funzionano miPiace e nonMiPiace

--- php/nonMiPiace.php
+++ php/nonMiPiace.php
@@ -5,8 +5,10 @@ include('../conf/db_config.php');
 $id_utenteVisto = $_GET['id'];
 
 
-$stmt = $conn->prepare("INSERT INTO `utente_visto_utente`(`idUtente`, `idUtenteVisto`, `mi_piace`) VALUES ('?','?','?')");
-$stmt->bind_param("sss",$_SESSION['id'], $id_utenteVisto, 1);
+$stmt = $conn->prepare("INSERT INTO `utente_visto_utente`(`idUtente`, `idUtenteVisto`, `mi_piace`) VALUES (?,?,0)");
+$stmt->bind_param("ss",$_SESSION['id'], $id_utenteVisto);
 $stmt->execute();
+
+header("location: ../home.php");
 
 ?>
